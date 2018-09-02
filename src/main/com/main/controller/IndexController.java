3556ed1ac5a9b0ff862a16aa99f5fa80c289d645
yang_yunxiang <password>
2018-09-02 23:42:06 +0800

    spring aop

--- src/main/com/main/controller/IndexController.java
+++ src/main/com/main/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.main.controller;
 
 import com.main.service.CommonService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 
 @RestController
@@ -27,9 +33,11 @@ public class IndexController {
         return "THIS IS A TEST.WELCOME" + type;
     }
 
-    /*@RequestMapping("getData")
-    public String getData() {
-        return indexService.findUser();
-    }*/
+    @Test
+    public void testHomePage() throws Exception {
+        ProfileController controller = new ProfileController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
 
 }
